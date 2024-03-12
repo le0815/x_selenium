a1c9aa8e1e3b8f4d664d0a7414ae5eaedd258638
admin <admin@email.com>
2024-03-13 03:03:56 +0700

    rename file to copilot.py

--- copilot.py
+++ copilot.py
@@ -6,9 +6,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
-usr_name = "@NgCHn22001358"
-pwd = "quyetdoanlen"
-
 
 def ClickBtn(driver, div_type, text, time_sleep=3):
     elem = driver.find_element(By.XPATH, f"//{div_type}[text()='{text}']")
@@ -23,31 +20,13 @@ def WriteInput(driver, div_inp_type, class_inp_name, div_submit_type, text_submi
     ClickBtn(driver, div_submit_type, text_submit_name)
 
 
-def Login(driver):
-    driver.get("https://twitter.com/i/flow/login")
-    time.sleep(3)
-    # input usr_name and submit
-    print("write usr_name")
-    WriteInput(driver, "input",
-               "r-30o5oe r-1dz5y72 r-13qz1uu r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-t60dpp r-fdjqy7",
-               "span",
-               "Next",
-               text=usr_name)
-    # input pwd and login
-    print("write pwd")
-    WriteInput(driver, "input",
-               "r-30o5oe r-1dz5y72 r-13qz1uu r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-t60dpp r-fdjqy7",
-               "span",
-               "Log in",
-               text=pwd)
-
-
 if __name__ == "__main__":
     driver = webdriver.Chrome()
     driver.get("https://copilot.microsoft.com/")
     time.sleep(3)
     # shadow_root -> https://www.youtube.com/watch?v=OhGY_ZNBsu0
-    shadow_root = driver.find_element(By.CSS_SELECTOR, "cib-serp.cib-serp-main").shadow_root.find_element(By.CSS_SELECTOR, "cib-action-bar").shadow_root.find_element(By.CSS_SELECTOR, "cib-text-input").shadow_root
+    shadow_root = driver.find_element(By.CSS_SELECTOR, "cib-serp.cib-serp-main").shadow_root.find_element(
+        By.CSS_SELECTOR, "cib-action-bar").shadow_root.find_element(By.CSS_SELECTOR, "cib-text-input").shadow_root
     elem = shadow_root.find_element(By.CLASS_NAME, "text-area")
     elem.send_keys("test")
     print("ok")
