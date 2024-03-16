63d48cadfa5773a79a2cf3728e83c9bd3f1f37ef
admin <admin@email.com>
2024-03-16 15:11:17 +0700

    fix bug in GetTrend, SetCookie, Tweet, main module

--- x.py
+++ x.py
@@ -1,5 +1,8 @@
 import pickle
 import time
+
+import pyperclip
+
 import copilot
 import json
 import asyncio
@@ -10,10 +13,13 @@ from selenium.webdriver.common.by import By
 
 __usr_name = "@NgCHn22001358"
 __pwd = "quyetdoanlen"
-__input_tweet = "//html/body/div/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div/div/div/div/div/label/div/div/div/div/div/div/div[2]/div/div/div/div"
-__post_prompt = "create twitter post  related to the hashtag: "
+# edge version
+# __input_tweet = "//html/body/div/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div/div/div/div/div/label/div/div/div/div/div/div/div[2]/div/div/div/div"
+# chrome version
+__input_tweet = "div.public-DraftStyleDefault-block.public-DraftStyleDefault-ltr"
+__post_prompt = "create twitter post less than 250 characters related to the hashtag: "
 __img_prompt = "create a image with hashtag: tiktok"
-
+__ad_link = "\nhttps://bit.ly/XHotNews"
 
 def ClickBtn(driver, div_type, text, time_sleep=3):
     elem = driver.find_element(By.XPATH, f"//{div_type}[text()='{text}']")
@@ -48,7 +54,7 @@ def Login(driver):
                text=__pwd, time_sleep=1)
 
 
-def Scroll(driver):
+async def Scroll(driver):
     # return homepage
     driver.get("https://twitter.com/home")
     # if appear popup require 2FA -> close pop up
@@ -66,13 +72,13 @@ def Scroll(driver):
     last_height = driver.execute_script("return document.body.scrollHeight")
     # scroll 10 times
     scroll_times = 0
-    while scroll_times <= 3:
+    while scroll_times <= 1:
         # Scroll down to bottom
         for i in range(8, 0, -1):
             print(f"scroll time: {scroll_times}")
             driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight/{i});")
             # Wait to load page
-            time.sleep(2)
+            await asyncio.sleep(2)
             print(f"scroll loop: {i}")
 
         # Calculate new scroll height and compare with last scroll height
@@ -85,16 +91,18 @@ def Scroll(driver):
 
     print("scroll finish")
     # refresh page to turn back top page
+    print('refreshing page')
     driver.refresh()
+    await asyncio.sleep(5)
 
 
-def GetTrend(driver):
+async def GetTrend(driver):
     # get list trends
     print("redirect to trending page")
     driver.get("https://twitter.com/i/trends")
     print("crawling trending")
     # wait for page loading
-    time.sleep(3)
+    await asyncio.sleep(3)
     trending_title_list = []
     elms = driver.find_elements(By.XPATH,
                                 "//div[@class='css-1rynq56 r-bcqeeo r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-b88u0q r-1bymd8e']")
@@ -115,50 +123,56 @@ def GetCookie(driver):
             break
 
 
-def SetCookie(driver):
+async def SetCookie(driver):
     driver.get("https://twitter.com/home")
-    time.sleep(3)
+    await asyncio.sleep(3)
     print("setting cookie")
     cookies = pickle.load(open("cookies.pkl", "rb"))
     for cookie in cookies:
         driver.add_cookie(cookie)
-    time.sleep(1)
+    await asyncio.sleep(1)
     print("setting cookie success -> redirect to the login page")
     driver.get("https://twitter.com/home")
 
 
-def Tweet(driver, post):
+async def Tweet(driver, post):
     print("insert post to tweet")
-    elem = driver.find_element(By.XPATH, __input_tweet)
-    time.sleep(1)
-    elem.send_keys(post)
-
+    elem = driver.find_element(By.CSS_SELECTOR, __input_tweet)
+    await asyncio.sleep(1)
+    pyperclip.copy(post)
+    elem.send_keys(Keys.CONTROL+'v')
+    await asyncio.sleep(1)
+    elem.send_keys(Keys.CONTROL + Keys.ENTER)
+    await asyncio.sleep(5)
+    print('posted')
 
 async def main():
-    SetCookie(driver)
-    GetTrend(driver)
-    trending_title_list = GetTrend(driver)
+    await SetCookie(driver)
+    trending_title_list = await GetTrend(driver)
     loop_count = len(trending_title_list)
     for trending_title in trending_title_list:
-        print(f"loop: {loop_count}")
-        loop_count += 1
+        print(f"loop for tweet: {loop_count}")
+        loop_count -= 1
         task_1 = asyncio.create_task(Scroll(driver))
         task_2 = asyncio.create_task(copilot.GetGeneratedPost(driver_2, __post_prompt + trending_title))
         tasks = [task_1, task_2]
+
         # wait for all task are complete
         done, pending = await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
 
         # iterator finished task
-        for future in pending:
-            print("pending task")
-            result = future.get_name()
+        # for future in pending:
+        #     print("pending task")
+        #     result = future.get_name()
 
         # iterator finished task
         for future in done:
             result = future.result()
+            print(f"async result type: {result} \n name: {future.get_name()}")
             if result is not None:
-                Tweet(driver, 'test')
+                await Tweet(driver, result+__ad_link)
 
+    print(f"posted {loop_count}: tweet")
 
 if __name__ == "__main__":
     driver = webdriver.Chrome()
